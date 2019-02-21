1a4920d628082c50de6e1e0bb4295f999f4a816c
Paul Guszak <pguszak@yahoo.com>
2019-02-21 11:18:19 -0700

    add code and fix errors

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
-var http = require('http');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var authController = require('./auth');
-var authJwtController = require('./auth_jwt');
+var authJwtController = require('./authjwt');
 db = require('./db')(); //global hack
 var jwt = require('jsonwebtoken');
 
@@ -15,6 +14,7 @@ app.use(passport.initialize());
 
 var router = express.Router();
 
+/*
 router.route('/post')
     .post(authController.isAuthenticated, function (req, res) {
             console.log(req.body);
@@ -25,23 +25,64 @@ router.route('/post')
             }
             res.send(req.body);
         }
-    );
-
-router.route('/postjwt')
-    .post(authJwtController.isAuthenticated, function (req, res) {
-            console.log(req.body);
-            res = res.status(200);
-            if (req.get('Content-Type')) {
-                console.log("Content-Type: " + req.get('Content-Type'));
-                res = res.type(req.get('Content-Type'));
+    );*/
+router.route('/movies')
+    .post(function (req, res) {
+        console.log(req.body);
+        //          no need for the below line just yet I think...
+        //          res.json({success: db.save(req.body), message: "movie saved", status:200, headers: req.headers, query: req.query, env: process.env.SECRET_KEY});
+        res.json({ message: "movie saved", status: 200, headers: req.headers, query: req.query, env: process.env.SECRET_KEY });
+    }
+    )
+    .get(function (req, res) {
+        /*
+        wasting my time lol...
+        var id = req.params.id.value; //req.swagger contains the path parameters
+        if(id != null){
+            var foundMov = db.find(id);
+            if(foundMov) {
+                res.json({status: 200, message: "GET movies", headers: req.headers, query: req.query, env: process.env.SECRET_KEY, movie: foundMov});
+            }else {
+                res.status(204).send();
             }
-            res.send(req.body);
+        }else{
+            res.json({ status: 200, message: "GET all movies", headers: req.headers, query: req.query, env: process.env.SECRET_KEY, movie: db.find()});
         }
-    );
+         */
+        res.json({ message: "GET movie", status: 200, headers: req.headers, query: req.query, env: process.env.SECRET_KEY });
+    }
+    )
+    .put(authJwtController.isAuthenticated, function (req, res) {
+        console.log(req);
+        /*        var id = req.params.id.value;
+                if(id != null){
+                }
+                when I realized I don't have to actually make this work yet.
+                */
+        res.json({ message: "movie updated", status: 200, headers: req.headers, query: req.query, env: process.env.SECRET_KEY });
 
-router.post('/signup', function(req, res) {
+    })
+    .delete(function (req, res) {
+        var user = db.findOne(req.body.username);
+
+        if (!user) {
+            res.status(401).send({ success: false, msg: 'Authentication failed. User not found.' });
+        } else {
+            //NGL I just ripped this from below, pretty sure this is Basic Auth and I didn't see a reason to reinvent the wheel...
+            if (req.body.password == user.password) {
+                var userToken = { id: user.id, username: user.username };
+                var token = jwt.sign(userToken, process.env.SECRET_KEY);
+                res.json({ message: "movie deleted", status: 200, headers: req.headers, query: req.query, env: process.env.SECRET_KEY });
+            }
+            else {
+                res.status(401).send({ success: false, msg: 'Authentication failed. Wrong password.' });
+            }
+        }
+    });
+
+router.post('/signup', function (req, res) {
     if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please pass username and password.'});
+        res.json({ success: false, msg: 'Please pass username and password.' });
     } else {
         var newUser = {
             username: req.body.username,
@@ -49,28 +90,33 @@ router.post('/signup', function(req, res) {
         };
         // save the user
         db.save(newUser); //no duplicate checking
-        res.json({success: true, msg: 'Successful created new user.'});
+        res.json({ success: true, msg: 'Successful created new user.' });
     }
 });
 
-router.post('/signin', function(req, res) {
+router.post('/signin', function (req, res) {
 
+    console.log(req.body);
     var user = db.findOne(req.body.username);
 
     if (!user) {
-        res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
+        res.status(401).send({ success: false, msg: 'Authentication failed. User not found.' });
     }
     else {
         // check if password matches
-        if (req.body.password == user.password)  {
-            var userToken = { id : user.id, username: user.username };
+        if (req.body.password == user.password) {
+            var userToken = { id: user.id, username: user.username };
             var token = jwt.sign(userToken, process.env.SECRET_KEY);
-            res.json({success: true, token: 'JWT ' + token});
+            res.json({ success: true, token: 'JWT ' + token });
         }
         else {
-            res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+            res.status(401).send({ success: false, msg: 'Authentication failed. Wrong password.' });
         }
     };
+});
+
+router.all('*', function (req, res) {
+    res.json({ error: 'Not supported HTTP method' });
 });
 
 app.use('/', router);
